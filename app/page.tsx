2ad0d4a6501b6bb39937e2727a48bfdfe8705f2f
raa decors <raadecors24@gmail.com>
2025-06-09 00:53:22 +0530

    raa 4 commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,17 @@
-import Image from "next/image";
-import Sample from "./componentss/sample";
+// app/page.tsx
+export const metadata = {
+  title: "RAA Decors",
+  icons: {
+    icon: "/images/raa-logo.png", // public/images/raa-logo.png
+  },
+};
+
 import Main from "./componentss/Main";
 
 export default function Home() {
-  return (<>
-    <Main />
-  </>);
+  return (
+    <>
+      <Main />
+    </>
+  );
 }
